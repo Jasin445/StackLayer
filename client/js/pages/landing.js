9f5d64c4bafd9d328fbdef582f1a07d43639a754
Jason Dagana <j.dagana@teflonhub.com>
2026-03-09 00:47:21 +0100

    added all screens for flow

---
+++ client/js/pages/landing.js
@@ -0,0 +1,23 @@
+// const getStartedBtn = document.querySelector("#get_started");
+// const getStartedBtn2 = document.querySelector("#get_started2");
+
+// const routes = {
+//     "index.html": "./index",
+//     "work-selection": "./pages/work-selection.html"
+// }
+
+// const goTo = (route ) => {
+//     const url = routes[route];
+
+//     fetch(url).then((res) => res.text())
+//         .then(html => {
+//             document.body.innerHTML = html
+//         history.pushState({}, "", "/" + route)
+//     })
+// }
+
+
+
+// getStartedBtn.addEventListener('click', () => goTo("work-selection"))
+// getStartedBtn2.addEventListener('click', () => goTo("work-selection"))
+
---
+++ client/js/pages/router.js
@@ -0,0 +1,44 @@
+const app = document.getElementById("app");
+
+const routes = {
+  "/": "./pages/landing-page.html",
+  "/work-selection": "./pages/work-selection.html",
+  "/tools": "./pages/project-phase.html",
+  "/ai-suggestion": "./pages/ai-suggestion.html",
+  "/ai-details": "./pages/ai-details.html",
+  "/discover": "./pages/discover.html"
+};
+
+ function router() {
+
+  const path = location.hash.replace("#", "") || "/";
+
+  const route = routes[path] || routes["/"];
+
+    fetch(route).then(res => res.text()).then(html => {
+
+      app.innerHTML = html;
+  
+      history.pushState({}, "", path)
+    }).catch(err => "<h1>404 Page Not Found</h1>");
+
+}
+
+document.addEventListener("click", (e) => {
+
+  const link = e.target.closest("[data-link]");
+  console.log(link)
+
+  if (!link) throw Error("Hello Bad");
+
+  const path = link.getAttribute("data-link");
+
+  location.hash = path;
+
+});
+
+window.addEventListener("hashchange", router);
+
+window.addEventListener("popstate", router)
+
+router();
